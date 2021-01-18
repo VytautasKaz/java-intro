9f34fed811f0b10ed84bcbfdd257247b96408b65
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-18 18:59:54 +0200

    triangles initial commit

---
+++ src/Triangles/Triangles.java
@@ -0,0 +1,11 @@
+package Triangles;
+
+public class Triangles {
+    public static void main(String[] args) {
+        int[][] t = {{3, 4, 5}, {2, 10, 8}, {5, 6, 5}, {5, 5, 5}};
+        for (int i = 0; i < t.length; i++) {
+            for (int j = 0; j < t[i].length; j++) {
+            }
+        }
+    }
+}
