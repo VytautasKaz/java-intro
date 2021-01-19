a1ee24aef44a85f7290ac3992370cb7576b98331
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-19 13:35:13 +0200

    determines the type of triangle

--- src/Triangles/Triangles.java
+++ src/Triangles/Triangles.java
@@ -2,9 +2,19 @@ package Triangles;
 
 public class Triangles {
     public static void main(String[] args) {
-        int[][] t = {{3, 4, 5}, {2, 10, 8}, {5, 6, 5}, {5, 5, 5}};
-        for (int i = 0; i < t.length; i++) {
-            for (int j = 0; j < t[i].length; j++) {
+        int[][] tri = {{3, 4, 5}, {2, 10, 8}, {5, 6, 5}, {5, 5, 5}};
+        for (int i = 0; i < tri.length; i++) {
+            int[] t = tri[i];
+            if (t[0] < t[1] + t[2] && t[1] < t[0] + t[2] && t[2] < t[0] + t[1]) {
+                if (t[0] == t[1] && t[1] == t[2]) {
+                    System.out.println("Equilateral triangle.");
+                } else if (t[0] == t[1] || t[0] == t[2] || t[1] == t[2]) {
+                    System.out.println("Isosceles triangle.");
+                } else {
+                    System.out.println("Scalene triangle.");
+                }
+            } else {
+                System.out.println("Invalid triangle.");
             }
         }
     }
