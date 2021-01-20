32fe38cfcd83db3ca685d2e5dd25717b96e8f120
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-20 17:17:14 +0200

    array sorted in descending order using sort method

--- src/DescendingSort/DescendingSort.java
+++ src/DescendingSort/DescendingSort.java
@@ -3,13 +3,15 @@ import java.util.*;
 
 public class DescendingSort {
     public static void main(String[] args) {
-        int[] array = {-10, 0, 2, 9, -5};
-        int number = array[0];
-        for (int i = 1; i < array.length; i++){
-            if (array[i] > number) {
-                number = array[i];
-            }
-        }
-        System.out.println(number);
+        Integer[] array = {-10, 0, 2, 9, -5};
+        Arrays.sort(array, Collections.reverseOrder());
+        System.out.printf("Modified arr[] : %s", Arrays.toString(array));
+//        int number = array[0];
+//        for (int i = 1; i < array.length; i++){
+//            if (array[i] > number) {
+//                number = array[i];
+//            }
+//        }
+//        System.out.println(number);
     }
 }
