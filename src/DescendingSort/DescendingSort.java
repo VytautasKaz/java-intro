270e5b62c659f1af7a1218cb221fbe8daaaeefd7
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-22 14:03:23 +0200

    array descending order sort algo

--- src/DescendingSort/DescendingSort.java
+++ src/DescendingSort/DescendingSort.java
@@ -3,15 +3,20 @@ import java.util.*;
 
 public class DescendingSort {
     public static void main(String[] args) {
-        Integer[] array = {-10, 0, 2, 9, -5};
-        Arrays.sort(array, Collections.reverseOrder());
-        System.out.printf("Modified arr[] : %s", Arrays.toString(array));
-//        int number = array[0];
-//        for (int i = 1; i < array.length; i++){
-//            if (array[i] > number) {
-//                number = array[i];
-//            }
-//        }
-//        System.out.println(number);
+//        Integer[] array = {-10, 0, 2, 9, -5};
+//        Arrays.sort(array, Collections.reverseOrder());
+//        System.out.printf("Modified arr[] : %s", Arrays.toString(array));
+//
+        int[] array = {-10, 0, 2, 9, -5};
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] < array[j]){
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
     }
 }
