647591ac2d6dba7d98e26a056ab8f1bec4f4fa83
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-24 14:18:00 +0200

    multiplication table initial commit

--- src/multTable/MultTable.java
+++ src/multTable/MultTable.java
@@ -2,11 +2,9 @@ package multTable;
 
 public class MultTable {
     public static void main(String[] args) {
-        int i = 1;
-        int j = 1;
         int e = 0;
-        for (i = 1; i <= 10; i++) {
-            for (j = 1; j <= 10; j++){
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++){
                 e = i * j;
                 System.out.println(i + " * " + j + " = " + e);
             }
