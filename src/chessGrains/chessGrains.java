1ec40dc267c5a1e1d737d164167170daea7139e9
VytautasKaz <VytautasKaz@users.noreply.github.com>
2021-01-27 09:38:07 +0200

    changed variable name

--- src/chessGrains/chessGrains.java
+++ src/chessGrains/chessGrains.java
@@ -5,11 +5,11 @@ import java.math.BigInteger;
 public class chessGrains {
     public static void main (String[] args)
     {
-        BigInteger sum = new BigInteger("1");
+        BigInteger count = new BigInteger("1");
         for (int i = 1; i <= 64; i++)
         {
-            System.out.println("Square " + i + ": " + sum + " grains.");
-            sum = sum.multiply(new BigInteger("2"));
+            System.out.println("Square " + i + ": " + count + " grains.");
+            count = count.multiply(new BigInteger("2"));
         }
     }
 }
